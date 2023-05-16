a4a3d6b24d8c2e109d1cd2224acbc3322bcff2f8
Nathan <nathan_byrne96@hotmail.co.uk>
2023-05-16 19:07:37 +0100

    landing page style changes

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:open_course/navigation.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,68 +16,76 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
         backgroundColor: const Color(0xff171717),
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(100),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text(
-                        "Open Course",
-                        style: TextStyle(
-                            fontSize: 30,
-                            color: Color(0xFFFFFFFF),
-                            fontWeight: FontWeight.bold),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Navigation()),
-                          );
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.fromLTRB(30, 6, 30, 6),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(
-                                  width: 2, color: const Color(0xff49C29D))),
-                          child: Row(
-                            children: const [
-                              Icon(
-                                Icons.login,
-                                color: Color(0xff49C29D),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                "Log in",
-                                style: TextStyle(
-                                    color: Color(0xff49C29D),
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16),
-                              )
-                            ],
+          child: ClipRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          const Text(
+                            "Open Course",
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Color(0xFFFFFFFF),
+                                fontWeight: FontWeight.bold),
                           ),
-                        ),
-                      )
-                    ],
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Navigation()),
+                              );
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.fromLTRB(30, 6, 30, 6),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(
+                                      width: 2, color: const Color(0xff49C29D))),
+                              child: Row(
+                                children: const [
+                                  Icon(
+                                    Icons.login,
+                                    color: Color(0xff49C29D),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    "Log in",
+                                    style: TextStyle(
+                                        color: Color(0xff49C29D),
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
-        body: Stack(
+        body: 
+        Stack(
           children: [
             Stack(
               children: [
@@ -98,65 +108,68 @@ class _LandingPageState extends State<LandingPage> {
                 ))),
               ],
             ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(20, 120, 20, 0),
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Learn.",
-                      style: TextStyle(
-                          color: Color(0xffffffff),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 90),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      "Your way.",
-                      style: TextStyle(
-                          color: Color(0xff49C29D),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 90),
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    Container(
-                      width: 330,
-                      child: RichText(
-                        text: TextSpan(
-                          children: [
-                            const TextSpan(
-                                text:
-                                    'Create courses from educational content available on YouTube, and track your progress as you learn ',
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Color.fromRGBO(255, 255, 255, .6),
-                                    height: 1.5)),
-                            TextSpan(
-                              text: '🌟',
-                              style: GoogleFonts.notoColorEmoji(),
-                            ),
-                          ],
+            ListView(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
+              children: [Container(
+                padding: const EdgeInsets.fromLTRB(20, 120, 20, 0),
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Learn.",
+                        style: TextStyle(
+                            color: Color(0xffffffff),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 90),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Your way.",
+                        style: TextStyle(
+                            color: Color(0xff49C29D),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 90),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Container(
+                        width: 330,
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              const TextSpan(
+                                  text:
+                                      'Create courses from educational content available on YouTube, and track your progress as you learn ',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Color.fromRGBO(255, 255, 255, .6),
+                                      height: 1.5)),
+                              TextSpan(
+                                text: '🌟',
+                                style: GoogleFonts.notoColorEmoji(),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(45, 5, 45, 5),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(6),
-                          color: const Color(0xff49C29D)),
-                      child: const Text("Sign up"),
-                    )
-                  ],
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(45, 5, 45, 5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            color: const Color(0xff49C29D)),
+                        child: const Text("Sign up"),
+                      )
+                    ],
+                  ),
                 ),
-              ),
+              )],
             )
           ],
         ));
